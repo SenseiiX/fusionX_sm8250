aa2ce4027d82d6e9e0d76bbe64655cf2676f1e84
SenseiiX <senseixdev@gmail.com>
2025-08-21 21:01:12 +0800

    treewide: Adding susfs v1.5.5
    Signed-off-by: SenX <senseiix@dev.com>

--- nextpatch.sh
+++ nextpatch.sh
@@ -1,5 +1,2 @@
 rm -rf KernelSU-Next
-curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -s v1.0.9
-echo "Adding more manager support..."
-cp -rf "ksun_more_manager.patch" KernelSU-Next && cd ./KernelSU-Next && patch -p1 -f < "ksun_more_manager.patch" && cd ..
-echo "Done (KSU, MKSU, RKSU, Fork by backslashxx and SukiSU)"
+curl -LSs "https://raw.githubusercontent.com/re-noroi/KernelSU-Next/next-susfs_v1.5.5-v1.5.7/kernel/setup.sh" | bash -s next-susfs_v1.5.5-v1.5.7
