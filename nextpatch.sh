b783df133bac04f00e2975dd353bc2adab9ed76f
SENX | センクス <senseixdev@gmail.com>
2025-09-19 05:52:38 +0000

    nextpatch: switch to @re-noroi 's fork
    Signed-off-by: SenX <senseiix@dev.com>

--- nextpatch.sh
+++ nextpatch.sh
@@ -1,5 +1,4 @@
 rm -rf KernelSU-Next
-curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -s v1.0.9
+curl -LSs "https://raw.githubusercontent.com/re-noroi/KernelSU-Next/next/kernel/setup.sh" | bash -s next
 echo "Adding more manager support..."
-cp -rf "ksun_more_manager.patch" KernelSU-Next && cd ./KernelSU-Next && patch -p1 -f < "ksun_more_manager.patch" && cd ..
-echo "Done (KSU, MKSU, RKSU, Fork by backslashxx and SukiSU)"
+echo "Done (KSU, MKSU, RKSU, Fork by backslashxx, SukiSU and WKSU)"
