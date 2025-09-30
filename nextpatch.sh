c326698c5b3954778a08de60bb2d35cf4d69e18c
Uday <forteciipher@gmail.com>
2025-09-30 23:02:50 +0530

    KernelSU: Setup @SukiSU-Ultra
    Signed-off-by: Uday <forteciipher@gmail.com>

--- nextpatch.sh
+++
@@ -1,4 +0,0 @@
-rm -rf KernelSU-Next
-curl -LSs "https://raw.githubusercontent.com/re-noroi/KernelSU-Next/susfs/kernel/setup.sh" | bash -s susfs
-echo "Adding more manager support..."
-echo "Done (KSU, MKSU, RKSU, Fork by backslashxx, SukiSU and WKSU)"
---
+++ setup_sukisu.sh
@@ -0,0 +1,3 @@
+rm -rf KernelSU
+echo "Setting up SukiSU-Ultra"
+curl -sSL "https://github.com/liyafe1997/SukiSU-Ultra/raw/4ff14cf0051d04209c4abd5027d99d8e7780ef5b/kernel/setup.sh" | bash -s f4863b20cc8dc0f8cc67418980f022e43014b598
