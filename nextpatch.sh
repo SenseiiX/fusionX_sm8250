68ee1aae5fa57ce4f7c9ca3cf33a746aefc7b522
SenseiiX <senseixdev@gmail.com>
2026-01-09 20:05:47 +0800

    treewide: Build KernelSU-Next 3.0 Legacy
    Change-Id: I7648ee2f88403de3649803f0a369b7ee9fb5f451
    Signed-off-by: SenseiiX <senseixdev@gmail.com>

--- nextpatch.sh
+++ nextpatch.sh
@@ -1,4 +1,2 @@
 rm -rf KernelSU-Next
-curl -LSs "https://raw.githubusercontent.com/SenseiiX/KernelSU-Next/next/kernel/setup.sh" | bash -s next
-echo "Adding more manager support..."
-echo "Done (KSU, MKSU, RKSU, Fork by backslashxx, SukiSU and WKSU)"
+curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s legacy
